45cd96b153d697c04da309fcf9aef4a3385dc58f
Cemre Yıldız <100775818+Cemrdeniz@users.noreply.github.com>
2026-06-12 18:58:00 +0300

    add layout login and dashboard

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+function Layout() {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
